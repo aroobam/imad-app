13450783897b6e8bf5c38e1cd6556424718cdce6
aroobam <30663615+aroobam@users.noreply.github.com>
2017-08-23 21:47:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/:articleName', function(req, res){
 
 function hash(input, salt){
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toSring('hex');
+    return hashed;
 }
 
 app.get('/hash/:input', function(req, res){
