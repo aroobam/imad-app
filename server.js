fe783ea0535db5c3bc4f27edcfba542b38817355
aroobam <30663615+aroobam@users.noreply.github.com>
2017-08-26 18:33:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,10 +127,37 @@ app.post('/create-user', function(req, res){
     
     pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result){
         if(err){
+            res.status(500).send(err.toString() + ' ');
+            
+        } else {
+            res.send("User successfully created: " + username + ' ');
+        }
+    });
+});
+
+app.post('/login', function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+
+   pool.query('SELECT * FROM "user" WHERE username=$1', [username], function(err, result){
+        if(err){
             res.status(500).send(err.toString());
             
         } else {
-            res.send("User successfully created: " + username);
+            if(result.rows.length === 0) {
+                res.status(403).send('username/password is invalid ');
+            }
+            else {
+                var dbString = result.rows[0].password;
+                var salt = dbString.split('$')[2];
+                var hashedPassword = hash(password, salt);
+                if( hashedPassword === dbString) {
+                    res.send('credentials correct ');
+                }
+                else {
+                res.status(403).send('username/password is invalid ');
+                }
+            }
         }
     });
 });
