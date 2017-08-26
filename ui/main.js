9a77f8b9359bd6bb7d4e38bbd861fcba46f297fc
aroobam <30663615+aroobam@users.noreply.github.com>
2017-08-26 18:56:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
 console.log('Loaded!');
+
+var submit = document.getElementById("submit");
+
+submit.onCLick = function(){
+    
+    //create a request object
+    var request = new XHttpRequest();
+    
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    
+    console.log(username);
+    console.log(password);
+    
+    request.open("POST", "http://aroobam.imad.hasura-app.io/login", true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(JSON.stringify({"username":username,"password":password}));
+}
